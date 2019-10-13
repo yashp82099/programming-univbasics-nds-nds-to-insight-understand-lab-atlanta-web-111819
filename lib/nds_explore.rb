edea4857d65a43b6d0f455067a4e4e776c85c63b
yash patel <yash82099@yahoo.com>
2019-10-13 23:29:21 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,16 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp nds 
   nil
 end
 
 def print_first_directors_movie_titles
+   fdmt = ['Jaws',"Close Encounters of the Third Kind", "Raiders of the Lost Ark", "E.T. the Extra-terrestrial","Schindler's List", "Lincoln"]
+   x = 0 
+   while x < fdmt.length do 
+     puts fdmt[x]
+     x += 1 
+   end 
+   
 end
