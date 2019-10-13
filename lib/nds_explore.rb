319469ffb364101632207093edeca63498b580e5
yash patel <yash82099@yahoo.com>
2019-10-13 23:27:32 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-   fdmt = ["Jaws","Close Encounters of the Third Kind", "Raider of the Lost Ark", "E.T. the Extra-ierrestrial","Schindler's List", "Lincoln"]
+   fdmt = ['Jaws',"Close Encounters of the Third Kind", "Raider of the Lost Ark", "E.T. the Extra-ierrestrial","Schindler's List", "Lincoln"]
    x = 0 
    while x < fdmt.length do 
      pp fdmt[x]
