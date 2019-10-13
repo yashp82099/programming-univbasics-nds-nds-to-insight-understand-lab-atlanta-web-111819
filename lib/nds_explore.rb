0f6c07d7fd3721e3478209c6dbc3ca4f202585f5
yash patel <yash82099@yahoo.com>
2019-10-13 23:26:41 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,5 +10,10 @@ end
 
 def print_first_directors_movie_titles
    fdmt = ["Jaws","Close Encounters of the Third Kind", "Raider of the Lost Ark", "E.T. the Extra-ierrestrial","Schindler's List", "Lincoln"]
-   pp fdmt
+   x = 0 
+   while x < fdmt.length do 
+     pp fdmt[x]
+     x += 1 
+   end 
+   
 end
